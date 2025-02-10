6c0efae44c9b967cdd953d564741c2bd1fb8d35e
Reiner.MS.CR <reiner1313@hotmail.com>
2025-02-09 22:37:35 -0600

    ACTUALIZACION CONTACTES E IMAGENES

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -5,14 +5,15 @@ const Footer = () => {
         <>
             <div className="flex justify-around items-center bg-gray-300 w-full">
 
-                <img className='h-20 w-20 p-2 rounded-2xl' src="/public/imgs/logo.png" alt="Municipalidad Pococi" />
+                <Link to={'/'}><img className='h-20 w-20 p-2 rounded-2xl' src="/imgs/logo.png" alt="Municipalidad Pococi" /></Link>
+                
 
                 <h1 className='hidden text-xl font-bold text-gray-500 lg:block'>Reconstrucción de la memoria histórica de Pococí</h1>
 
                 <div className='flex gap-4 lg:flex-row'>
                     <Link className='font-bold text-gray-500 hover:text-green-800' to="/">Inicio</Link>
                     <Link className='font-bold text-gray-500 hover:text-green-800' to="/">Aportar</Link>
-                    <Link className='font-bold text-gray-500 hover:text-green-800' to="/">Contáctenos</Link>
+                    <Link className='font-bold text-gray-500 hover:text-green-800' to="/contact">Contáctenos</Link>
                 </div>
             </div>
         </>
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "../pages/HomePage";
+import HomePage from "../pages/Homepage";
+import Contact from "../pages/Contact";
 import Layout from "../components/Layout";
 
 const AppRoutes = () => {
@@ -8,6 +9,7 @@ const AppRoutes = () => {
       <Layout>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/contact" element={<Contact/>} />
         </Routes>
       </Layout>
     </Router>
